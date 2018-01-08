d271c8607b921cbe118551ffc8a929ab4755daa9
Ramana <ramana.kambapu@gmail.com>
2018-01-08 11:56:40 +0530

    Demo Project Committed

---
+++ src/main/java/com/maven/demo_maven/App.java
@@ -0,0 +1,13 @@
+package com.maven.demo_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
